c7d946b9ebfdd23bd0e4a15446d878a4877a714b
Noemie Pierart <noemie.pierart@gmail.com>
2019-11-19 23:41:27 +0100

    Starting to add photos with uploader

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,3 +1,37 @@
 class OffersController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
+
+  def index
+    @offers = Offer.all
+  end
+
+  def show
+    @offer = Offer.find(params[:id])
+  end
+
+  def create
+    @offer = Offer.new(offer_params)
+    @offer.save
+  end
+
+  def edit
+    @offer = Offer.find(params[:id])
+  end
+
+  def update
+    @offer = Offer.find(params[:id])
+    @offer.update(offer_params)
+  end
+
+  def destroy
+    @offer = Offer.find(params[:id])
+    @offer.destroy
+  end
+
+  private
+
+  def offer_params
+    params.require(:offer).permit(:title, :description, :availability, :price, :active, :place, :category, :photo)
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,6 @@
 class UsersController < ApplicationController
+
+  def article_params
+    params.require(:user).permit(:email, :encrypted_password, :reset_password_token, :username, :phone_number, :first_name, :last_name, :photo)
+  end
 end
