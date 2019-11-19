f37c3b84d527cbb0fa9fb1a302f6764c3de4e48d
Noemie Pierart <noemie.pierart@gmail.com>
2019-11-19 22:43:01 +0100

    Setup Cloudinary - Done for Users - except in prod

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -5,4 +5,7 @@ class Offer < ApplicationRecord
   @categories = ["Plomberie", "Bricolage", "Jardinage", "Electricite", "Peinture", "Demenagement", "Couture", "Decoration", "Montage meubles", "Electromenager"]
 
   validates :category, inclusion: { in: @categories }
+
+  mount_uploader :photo, PhotoUploader
+
 end
