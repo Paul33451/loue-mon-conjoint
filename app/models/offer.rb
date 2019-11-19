2c381a713516d2da42c3b6fafd9aa2adfab1a80d
Valentin DAUSSE <valentindausse@gmail.com>
2019-11-19 11:30:39 +0100

    rectif model offers

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -2,11 +2,11 @@ class Offer < ApplicationRecord
   has_many :bookings
   belongs_to :user
 
-  validates :title, presence: true
-  validates :availability, presence: true
-  validates :price, presence: true
-  validates :active, presence: true
-  validates :place, presence: true
-  validates :category, presence: true
-  validates :user_id, presence: true, uniqueness: true
+  # validates :title, presence: true
+  # validates :availability, presence: true
+  # validates :price, presence: true
+  # validates :active, presence: true
+  # validates :place, presence: true
+  # validates :category, presence: true
+  # validates :user_id, presence: true, uniqueness: true
 end
