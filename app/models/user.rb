793638f02ce3040fbbc95f981c4524c402d9ca14
Noemie Pierart <noemie.pierart@gmail.com>
2019-11-20 17:15:52 +0100

    POUBELLE A SUPPRIMER

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  mount_uploader :photo, PhotoUploader
+  mount_uploader :avatar, AvatarUploader
 end
